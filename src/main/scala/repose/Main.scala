6b75b1421ccbc14b553760aabe4f8ed92f4a5ef4
Philipp Ruemmer <ph_r@gmx.net>
2020-10-18 21:52:59 +0200

    visitors

--- src/main/scala/repose/Main.scala
+++ src/main/scala/repose/Main.scala
@@ -2,5 +2,8 @@
 package repose;
 
 object Main extends App {
-  println("Hello")
+  val trans = new Transformer
+
+  trans.transform("test/glob_78.smt2",
+                  "test/glob_78_rewritten.smt2")
 }
---
+++ src/main/scala/repose/Transformer.scala
@@ -0,0 +1,38 @@
+
+package repose;
+
+import ap.parser.smtlib._
+import ap.parser.smtlib.Absyn._
+
+import scala.{Option => SOption}
+import scala.collection.JavaConverters._
+
+class Transformer {
+
+  val printer = new PrettyPrinterNonStatic
+
+  def transform(inputFile : String, outputFile : String) : Unit = {
+    Console.err.println("Rewriting \"" + inputFile +
+                          "\" to \"" + outputFile + "\" ...")
+
+    val input =
+      new java.io.BufferedReader (
+        new java.io.FileReader(new java.io.File (inputFile)))
+    val l = new Yylex(input)
+    val p = new parser(l) {
+      override def report_error(message : String, info : Object) : Unit = {
+        Console.err.println(message)
+      }
+    }
+
+    val script = p.pScriptC
+
+    val out = new java.io.FileOutputStream(outputFile)
+    Console.withOut(out) {
+      println(printer print script)
+    }
+
+    out.close
+  }
+
+}
