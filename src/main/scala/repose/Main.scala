acda148c779571eb1b714456c95ec15b85593c33
Philipp Ruemmer <ph_r@gmx.net>
2020-11-05 15:25:28 +0100

    better error handling

--- src/main/scala/repose/Main.scala
+++ src/main/scala/repose/Main.scala
@@ -5,10 +5,14 @@ object Main {
 
   def main(args: Array[String]) : Unit = {
     val trans = new Transformer
-    for (inputFile <- args) {
-      val outputFile = inputFile + "-processed.smt2"
-      trans.transform(inputFile, outputFile)
-    }
+    for (inputFile <- args)
+      try {
+        val outputFile = inputFile + "-processed.smt2"
+        trans.transform(inputFile, outputFile)
+      } catch {
+        case e : Exception =>
+          Console.err.println(e.getMessage)
+      }
   }
 
 }
